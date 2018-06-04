91ad4658a0b03ada68e3bc1b796b6b42b0226529
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2018-06-05 00:52:01 +0200

    Replaced console output with logging

--- src/main/java/io/github/ilyazinkovich/scientist/ExperimentResultsLogger.java
+++ src/main/java/io/github/ilyazinkovich/scientist/ExperimentResultsLogger.java
@@ -4,8 +4,12 @@ import static java.lang.String.format;
 import static java.util.stream.Collectors.joining;
 
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExperimentResultsLogger {
+
+  private static final Logger log = LoggerFactory.getLogger(ExperimentResultsLogger.class);
 
   private final String experimentName;
 
@@ -17,6 +21,6 @@ public class ExperimentResultsLogger {
     final String resultsString = results.entrySet().stream()
         .map(entry -> format("%s: %d", entry.getKey().name(), entry.getValue()))
         .collect(joining(", "));
-    System.out.printf("%s results: %s%n", experimentName, resultsString);
+    log.info("{} results: {}", experimentName, resultsString);
   }
 }
